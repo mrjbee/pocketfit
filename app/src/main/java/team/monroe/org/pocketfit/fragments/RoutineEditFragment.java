b74df2e4abb082f33381d7fbce044dea1709b3da
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2015-05-15 13:01:04 +0300

    Add routine view template

--- app/src/main/java/team/monroe/org/pocketfit/fragments/RoutineEditFragment.java
+++ app/src/main/java/team/monroe/org/pocketfit/fragments/RoutineEditFragment.java
@@ -3,18 +3,13 @@ package team.monroe.org.pocketfit.fragments;
 
 import android.os.Bundle;
 
-import org.monroe.team.android.box.app.FragmentSupport;
-
-import team.monroe.org.pocketfit.Dashboard;
-import team.monroe.org.pocketfit.PocketFitApp;
 import team.monroe.org.pocketfit.R;
-import team.monroe.org.pocketfit.view.SlidingRelativeLayout;
 
 public class RoutineEditFragment extends BodyFragment {
 
     @Override
     protected int getLayoutId() {
-        return R.layout.fragment_routine_editor;
+        return R.layout.fragment_routines;
     }
 
     @Override
