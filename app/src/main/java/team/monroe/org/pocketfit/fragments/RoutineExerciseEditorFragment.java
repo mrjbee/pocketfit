1a44465aa4009ed161a881689cc0076d0422e170
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2015-05-19 14:04:18 +0300

    Add details UI

--- app/src/main/java/team/monroe/org/pocketfit/fragments/RoutineExerciseEditorFragment.java
+++ app/src/main/java/team/monroe/org/pocketfit/fragments/RoutineExerciseEditorFragment.java
@@ -1,11 +1,13 @@
 package team.monroe.org.pocketfit.fragments;
 
+import android.os.Bundle;
 import android.view.View;
 
 import team.monroe.org.pocketfit.PocketFitApp;
 import team.monroe.org.pocketfit.R;
 import team.monroe.org.pocketfit.presentations.Exercise;
 import team.monroe.org.pocketfit.presentations.RoutineExercise;
+import team.monroe.org.pocketfit.view.presenter.ViewPresenter;
 
 public class RoutineExerciseEditorFragment extends BodyFragment {
 
@@ -13,6 +15,42 @@ public class RoutineExerciseEditorFragment extends BodyFragment {
     private String mRoutineExerciseId;
     private String mExerciseId;
     private RoutineExercise mRoutineExercise;
+
+    private ExerciseDetailsViewPresenter<RoutineExercise.PowerExerciseDetails> powerExerciseDetailsViewPresenter;
+    private ExerciseDetailsViewPresenter<RoutineExercise.DistanceExerciseDetails> distanceExerciseDetailsViewPresenter;
+    private ExerciseDetailsViewPresenter<RoutineExercise.TimeExerciseDetails> timeExerciseDetailsViewPresenter;
+    private ExerciseDetailsViewPresenter<RoutineExercise.TimesExerciseDetails> timesExerciseDetailsViewPresenter;
+
+    private ExerciseDetailsViewPresenter mDetailsViewPresenter;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        powerExerciseDetailsViewPresenter = new ExerciseDetailsViewPresenter<RoutineExercise.PowerExerciseDetails>(view(R.id.panel_power)) {
+            @Override
+            public void fillDetails(RoutineExercise.PowerExerciseDetails details) {
+
+            }
+        };
+        distanceExerciseDetailsViewPresenter = new ExerciseDetailsViewPresenter<RoutineExercise.DistanceExerciseDetails>(view(R.id.panel_disatnce)) {
+            @Override
+            public void fillDetails(RoutineExercise.DistanceExerciseDetails details) {
+
+            }
+        };
+        timeExerciseDetailsViewPresenter = new ExerciseDetailsViewPresenter<RoutineExercise.TimeExerciseDetails>(view(R.id.panel_time)) {
+            @Override
+            public void fillDetails(RoutineExercise.TimeExerciseDetails details) {
+
+            }
+        };
+        timesExerciseDetailsViewPresenter = new ExerciseDetailsViewPresenter<RoutineExercise.TimesExerciseDetails>(view(R.id.panel_times)) {
+            @Override
+            public void fillDetails(RoutineExercise.TimesExerciseDetails details) {
+
+            }
+        };
+    }
 
     @Override
     protected boolean isHeaderSecondary() {
@@ -95,5 +133,31 @@ public class RoutineExerciseEditorFragment extends BodyFragment {
     private void updateExerciseDetailsUI() {
         view_text(R.id.text_title).setText(mRoutineExercise.exercise.title);
         view_text(R.id.text_description).setText(mRoutineExercise.exercise.description);
+        switch (mRoutineExercise.exercise.type){
+            case distance:
+                mDetailsViewPresenter = distanceExerciseDetailsViewPresenter;
+                break;
+            case time:
+                mDetailsViewPresenter = timeExerciseDetailsViewPresenter;
+                break;
+            case times:
+                mDetailsViewPresenter = timesExerciseDetailsViewPresenter;
+                break;
+            case weight_times:
+                mDetailsViewPresenter = powerExerciseDetailsViewPresenter;
+                break;
+            default:
+                throw new IllegalStateException();
+        }
+        mDetailsViewPresenter.show();
+    }
+
+    public static abstract class ExerciseDetailsViewPresenter<DetailsType extends RoutineExercise.ExerciseDetails> extends ViewPresenter<View>{
+
+        public ExerciseDetailsViewPresenter(View rootView) {
+            super(rootView);
+        }
+
+        public abstract void fillDetails(DetailsType details);
     }
 }
